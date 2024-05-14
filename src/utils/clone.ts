97fd96f5f52a1fd47a1cb32eb03d1b4f774bbc83
buduo <892360315@qq.com>
2024-05-14 19:32:57 +0800

    feat: 优化打印输出的样式

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -2,8 +2,8 @@
 import simpleGit, {SimpleGit, SimpleGitOptions} from 'simple-git';
 import createLogger from 'progress-estimator';
 import chalk from 'chalk';
-// const figlet = require('figlet');
-// import log from './log';
+const figlet = require('figlet');
+import log from './log';
 // 初始化进度条
 const logger = createLogger({
     spinner: {
@@ -15,10 +15,10 @@ const logger = createLogger({
     },
 });
 
-// const goodPrinter = async () => {
-//     const data = await figlet('buduo-cli');
-//     console.log(chalk.rgb(40, 156, 193).visible(data));
-// };
+const goodPrinter = async () => {
+    const data = await figlet('buduo-cli');
+    console.log(chalk.rgb(40, 156, 193).visible(data));
+};
 
 const gitOptions: Partial<SimpleGitOptions> = {
     baseDir: process.cwd(), // 当前工作目录
@@ -32,18 +32,17 @@ export const clone = async (url: string, projectName: string, options: string[])
             estimate: 7000, // 预计下载时间
         });
         // 下面就是一些相关的提示
-        // goodPrinter();
+        goodPrinter();
         console.log();
         console.log(chalk.blueBright(`==================================`));
         console.log(chalk.blueBright(`=== 欢迎使用 buduo-cli 脚手架 ===`));
         console.log(chalk.blueBright(`==================================`));
         console.log();
-
-        // log.success(`项目创建成功 ${chalk.blueBright(projectName)}`);
-        // log.success(`执行以下命令启动项目：`);
-        // log.info(`cd ${chalk.blueBright(projectName)}`);
-        // log.info(`${chalk.yellow('pnpm')} install`);
-        // log.info(`${chalk.yellow('pnpm')} run dev`);
+        log.success(`项目创建成功 ${chalk.blueBright(projectName)}`);
+        log.success(`执行以下命令启动项目：`);
+        log.info(`cd ${chalk.blueBright(projectName)}`);
+        log.info(`${chalk.yellow('pnpm')} install`);
+        log.info(`${chalk.yellow('pnpm')} run dev`);
     } catch (error) {
         // log.error(chalk.red('代码下载失败'));
         // console.log(error);
