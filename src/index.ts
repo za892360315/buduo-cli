10b027c63c7d5417b260d2ea8eb7acfc0a10cdef
buduo <892360315@qq.com>
2024-05-14 14:06:19 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {Command} from 'commander';
+import {version} from '../package.json';
+import {create} from './command/create';
+// import { update } from './command/update';
+const program = new Command('dawei');
+program.version(version, '-v --version');
+
+// program
+//     .command('update')
+//     .description('更新脚手架 dawei-cli')
+//     .action(async () => {
+//         await update();
+//     });
+
+program.command('create').description('创建一个新项目').argument('[name]', '项目名称').action(async (dirName) => {
+    create(dirName);
+    // if (dirName) {
+    //     create(dirName);
+    // } else {
+    //     console.log(`create ${dirName}`)
+    // }
+    });
+
+program.parse();
