2e8a78c3e51d3fd1cbff2bb26ce9dea71b25579d
buduo <892360315@qq.com>
2024-07-13 19:50:15 +0800

    fix: 调整2

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -34,7 +34,7 @@ export const clone = async (url: string, projectName: string, options: string[])
         goodPrinter();
         console.log();
         console.log(chalk.blueBright(`==================================`));
-        console.log(chalk.blueBright(`=== 欢迎使用 dawei-cli 脚手架 ===`));
+        console.log(chalk.blueBright(`=== 欢迎使用 buduo-cli 脚手架 ===`));
         console.log(chalk.blueBright(`==================================`));
         console.log();
 
